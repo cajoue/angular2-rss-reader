8de9fd6a5872156ee6428de1ace5005f75870a0e
cajoue <cajoue@users.noreply.github.com>
2017-02-07 09:54:29 +0100

    feat: Use FeedService to get Feed content
    to extract the content from the API to use in a Feed Observable

--- src/app/feed.service.ts
+++ src/app/feed.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Rx';
+
+import { Feed } from './model/feed';
 
 @Injectable()
 export class FeedService {
+  private rssToJsonBaseUrl: string = 'https://api.rss2json.com/v1/api.json?rss_url=';
 
-  constructor() { }
+  constructor( private http: Http ) { }
 
+  getFeedContent(url: string): Observable<Feed> {
+    return this.http.get(this.rssToJsonBaseUrl + url)
+                    .map(this.extractFeeds)
+                    .catch(this.handleError);
+  }
+
+  private extractFeeds(res: Response): Feed {
+    let feed = res.json();
+    return feed || {};
+  }
+
+  private handleError( error: any ){
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);  
+  }
 }
