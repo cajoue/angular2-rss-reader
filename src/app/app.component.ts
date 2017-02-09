c10e13bb2114e11601b179bd923cdf2e6a93ad34
cajoue <cajoue@users.noreply.github.com>
2017-02-09 11:29:39 +0100

    feat: Display RSS feed item list
    To load and display a specific RSS feed that has been converted to JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FeedService } from './feed.service';
+// Add the RxJS Observable operators we need in this app.
+import './rxjs-operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  /*private feedUrl: string = 'http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro';*/
+  // if necessary, process the required link on this page:
+  // https://rss2json.com and use the rss_url
+  /*private feedUrl: string = 'http%3A%2F%2Fwww.20min.ch%2Frss%2Frss.tmpl%3Ftype%3Dchannel%26get%3D20%26lang%3Dro';*/
+  private feedUrl: string = 'https://css-tricks.com/feed/';
+  /*private feedUrl: string = 'https://www.becompany.ch/en/blog/feed.xml';*/
+  /*private feedUrl: string = 'https://techcrunch.com/feed/';*/
+  /*private feedUrl: string = 'http://www.rockclimbing.com/rss/routes/ascents/new.xml';*/
+
+  private feeds: any;
+
+  constructor (
+    private feedService: FeedService
+  ) {}
+
+  ngOnInit() {
+    this.refreshFeed();
+  }
+
+  private refreshFeed() {
+    this.feedService.getFeedContent(this.feedUrl)
+                    .subscribe(
+                      feed => this.feeds = feed.items,
+                      error => console.log(error)
+                    );
+  }
 }
